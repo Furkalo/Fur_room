73c7025ef6a215d428d5ded7d539724532da1b0a
Daniil Furkalo <daniilf077@gmail.com>
2025-04-15 22:41:09 +0300

    fix

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -1,8 +1,6 @@
 import MeetingTypeList from "@/components/MeetingTypeList";
 
 const Home = () => {
-  const now = new Date();
-
   return (
     <section className="flex size-full flex-col gap-5 text-white">
       <MeetingTypeList />
